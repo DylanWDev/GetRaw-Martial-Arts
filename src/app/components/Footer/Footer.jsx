a4ec1409e02545295752e8a877c3b729a99f2a2e
DylanWDev <xpoweroutage@gmail.com>
2024-04-25 15:57:58 -0400

    started footer and contact page

---
+++ src/app/components/Footer/Footer.jsx
@@ -0,0 +1,20 @@
+export default function Footer() {
+  return (
+    <>
+        <div className=" bg-yellow-300 w-full"> 
+            <div className="container">
+                <div>
+                    <h2>Business Hours</h2>
+                    <p>Mon, Tue, Wed</p>
+                </div>
+                <div>
+
+                </div>
+                <div>
+
+                </div>
+            </div>
+        </div>
+    </>
+  );
+}
--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,12 +1,18 @@
 import Nav from "../components/Nav/Nav";
+import Footer from "../components/Footer/Footer";
 
 export default function contact() {
   return (
-    <div className="">
-      <div className="star-bg absolute star-container "></div>
-      <div className="absolute top-0 w-full">
-        <Nav />
+    <>
+    <div className="relative"> 
+        <div className="top-0 w-full bg-slate-600">
+          <Nav />
+        </div>
+        <div className="h-screen z-50">
+          <p>Hello World</p>
+        </div>
       </div>
-    </div>
+      <Footer />
+    </>
   );
 }
