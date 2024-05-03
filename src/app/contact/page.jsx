805551b16fd5a4da409ef8fa2924b5769d9cac28
DylanWDev <xpoweroutage@gmail.com>
2024-05-02 23:30:50 -0400

    changed padding on contact page

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -95,7 +95,7 @@ export default function Contact() {
             >
               Schedule My Free Class
             </button>
-            <p className="text-xs text-gray-400 p-1">
+            <p className="text-xs text-gray-400 pt-2">
               By opting into this webform you are giving consent for Brian
               Chewning's GetRaw Martial Arts to send you periodic text messages.
               Standard rates may apply. You may opt-out anytime by replying
