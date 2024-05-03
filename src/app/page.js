cbb6a67d31cf55707bdb61a1243e7e2cabac517b
DylanWDev <xpoweroutage@gmail.com>
2024-05-02 22:58:15 -0400

    added footer to main page

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Nav from "./components/Nav/Nav";
+import Footer from "./components/Footer/Footer";
 
 const Home = () => {
   return (
@@ -14,6 +15,7 @@ const Home = () => {
         <PhilosophySection />
         <Testimonials />
       </div>
+      <Footer />
     </>
   );
 };
